a3e80af28b8d663eace0dcf3c632b0ccb278c177
Chameleon Cloud User <cc@zharfanf-gpu.novalocal>
2023-05-15 06:30:28 +0000

    fixing file location

--- run-vap.sh
+++ run-vap.sh
@@ -11,8 +11,8 @@ cp -r ../dds-zharfanf/data-set/ ./src/app/dds-qyx/
 # rene
 cd src/app/dds-qyx/data-set/rene
 mkdir results
-for i in ../../../../../dds-zharfanf/workspace/results/*rene*; do cp $i ./results/; done
-cp -r ./../../../../dds-zharfanf/workspace/profile-rene/ .
+for i in ../../../../../../dds-zharfanf/workspace/results/*rene*; do cp $i ./results/; done
+cp -r ../../../../../../dds-zharfanf/workspace/profile-rene/profile-separated/ .
 mv profile-separated/ profile/
 cd profile/
 touch profile_info.yml
@@ -20,8 +20,8 @@ echo 'segment_size: 5' >> profile_info.yml
 
 cd ../uav-1/
 mkdir results
-for i in ../../../../../dds-zharfanf/workspace/results/*uav-1*; do cp $i ./results/; done
-cp -r ./../../../../dds-zharfanf/workspace/profile-uav-1/ .
+for i in ../../../../../../dds-zharfanf/workspace/results/*uav-1*; do cp $i ./results/; done
+cp -r ../../../../../../dds-zharfanf/workspace/profile-uav-1/profile-separated/ .
 mv profile-separated/ profile/
 cd profile/
 touch profile_info.yml
@@ -30,8 +30,8 @@ cd ..
 
 cd ../uav-2/
 mkdir results
-for i in ../../../../../dds-zharfanf/workspace/results/*uav-2*; do cp $i ./results/; done
-cp -r ./../../../../dds-zharfanf/workspace/profile-uav-2/ .
+for i in ../../../../../../dds-zharfanf/workspace/results/*uav-2*; do cp $i ./results/; done
+cp -r ../../../../../../dds-zharfanf/workspace/profile-uav-2/profile-separated/ .
 mv profile-separated/ profile/
 cd profile/
 touch profile_info.yml
@@ -42,8 +42,8 @@ cd ..
 
 cd ../jakarta/
 mkdir results
-for i in ../../../../../dds-zharfanf/workspace/results/*jakarta*; do cp $i ./results/; done
-cp -r ./../../../../dds-zharfanf/workspace/profile-jakarta/ .
+for i in ../../../../../../dds-zharfanf/workspace/results/*jakarta*; do cp $i ./results/; done
+cp -r ../../../../../../dds-zharfanf/workspace/profile-jakarta/profile-separated/ .
 mv profile-separated/ profile/
 cd profile/
 touch profile_info.yml
@@ -51,8 +51,8 @@ echo 'segment_size: 5' >> profile_info.yml
 
 cd ../highway/
 mkdir results
-for i in ../../../../../dds-zharfanf/workspace/results/*highway*; do cp $i ./results/; done
-cp -r ./../../../../dds-zharfanf/workspace/profile-highway/ .
+for i in ../../../../../../dds-zharfanf/workspace/results/*highway*; do cp $i ./results/; done
+cp -r ../../../../../../dds-zharfanf/workspace/profile-highway/profile-separated/ .
 mv profile-separated/ profile/
 cd profile/
 touch profile_info.yml
@@ -61,8 +61,8 @@ cd ..
 
 cd ../coldwater/
 mkdir results
-for i in ../../../../../dds-zharfanf/workspace/results/*coldwater*; do cp $i ./results/; done
-cp -r ./../../../../dds-zharfanf/workspace/profile-coldwater/ .
+for i in ../../../../../../dds-zharfanf/workspace/results/*coldwater*; do cp $i ./results/; done
+cp -r ../../../../../../dds-zharfanf/workspace/profile-coldwater/profile-separated/ .
 mv profile-separated/ profile/
 cd profile/
 touch profile_info.yml
@@ -71,8 +71,8 @@ cd ..
 
 cd ../roppongi/
 mkdir results
-for i in ../../../../../dds-zharfanf/workspace/results/*roppongi*; do cp $i ./results/; done
-cp -r ./../../../../dds-zharfanf/workspace/profile-roppongi/ .
+for i in ../../../../../../dds-zharfanf/workspace/results/*roppongi*; do cp $i ./results/; done
+cp -r ../../../../../../dds-zharfanf/workspace/profile-roppongi/profile-separated/ .
 mv profile-separated/ profile/
 cd profile/
 touch profile_info.yml
